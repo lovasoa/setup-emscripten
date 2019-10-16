93cd0f8707ee2d1e90cb8b23ecce7b022456d19b
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-10-16 11:59:04 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# setup-emscripten
-emscripten github action
+# emscripten github action
+
+This action installs emscripten
+
+## Inputs
+
+### `emscripten-version`
+
+**optional** The version of emscripten to install
+
+## Example usage
+
+uses: lovasoa/setup-emscripten@master
+with:
+  emscripten-version: '1.38.47'
---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
+
+async function run(version) {
+  const emsdk_zip = await tc.download("https://github.com/emscripten-core/emsdk/archive/master.zip");
+  const extPath = await tc.extractZip(emsdk_zip);
+  const toolRoot = path.join(extPath, 'emsdk-master');
+  const emsdk_bin = path.join(toolRoot, 'emsdk');
+
+  await exec.exec(emsdk_bin, ["install", version]);
+  await exec.exec(emsdk_bin, ["activate", version]);
+  await exec.exec(path.join(extPath, 'emsdk-env.sh'));
+}
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const version = core.getInput('emscripten-version') || 'latest';
+  console.log(`Installing emscripten (${version})...`);
+  await run();
+} catch (error) {
+  core.setFailed(error.message);
+}
